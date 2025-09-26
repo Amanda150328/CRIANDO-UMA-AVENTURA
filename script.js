1a9526c2dfaafc24feabc2733f50b5da8941f02b
Amanda150328 <amanda.trukane.miranda@escola.pr.gov.br>
2025-09-26 10:31:17 -0300

    save

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const avança = document.querySelectorAll('.btn-proximo');
 
 avança.forEach(button => {
 button.addEventListener('click', function(){
-const atual = document.querySelector('.ativo')
+const atual = document.querySelector('.ativo');
+const proximoPasso = 'passo-' + this.getAttribute('data-proximo')
 })
 })
